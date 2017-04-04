5e46bbcdebc45da7b296f3c3b1bd07e7c1587bfb
Wazser <kaches1610@mail.ru>
2017-04-05 00:40:18 +0300

    Now it is scaling

--- main_menu.cpp
+++ main_menu.cpp
@@ -4,15 +4,16 @@
 #include "method.cpp"
 #include "celestial_body.cpp"
 #include "interface.cpp"
-#define WIDTH 1080
+#define WIDTH 600
 #define T_SCALE 1
 
 using namespace sf;
 
 int main()
 {
-	Celestial_Body Sun(0,0,0,0,0,0,20,1.9885e30), Earth(152.1e9,0,0,29.783e3,0,0,7,5.9726e24),Mercury(46e9, 0, 0, 58.98e3, 0, 0, 7, 0.33011e24), Venus(107.48e9, 0, 0, 35.26e3, 0, 0, 7, 4.8675e24);
-	RectangleShape settings(Size),exit(Size),load(Size),new1(Size),title(Vector2f(700,200));
+	float k=WIDTH/1080.0;
+	Celestial_Body Sun(0,0,0,0,0,0,20*k,1.9885e30), Earth(152.1e9,0,0,29.783e3,0,0,7*k,5.9726e24),Mercury(46e9, 0, 0, 58.98e3, 0, 0, 7*k, 0.33011e24), Venus(107.48e9, 0, 0, 35.26e3, 0, 0, 7*k, 4.8675e24);
+	RectangleShape settings(k*Size),exit(k*Size),load(k*Size),new1(k*Size),title(Vector2f(700*k,200*k));
 	Texture t1,t2,t3,t4,t5,t6,t7,t8,t9;
 	t1.loadFromFile("system files/settings.png");
 	t2.loadFromFile("system files/exit.png");
@@ -30,7 +31,7 @@ int main()
 	title.setTexture(&t9);
 	Atlas atl;
 	Clock clock;
-	RenderWindow window(VideoMode(1920,1080),"Star Forge");
+	RenderWindow window(VideoMode(1920*k,WIDTH),"Star Forge");
 	window.setMouseCursorVisible(false);
 	ConvexShape* shape = mouse_create();
 	Vector2i p;
@@ -44,11 +45,11 @@ int main()
 	atl.first->next->next->avatar.setTexture(&t7);
 	atl.first->next->next->next->avatar.setTexture(&t8);
 	Phase_vector v1,v2;
-	title.setPosition(LENGTH-100,10);
-	new1.setPosition(LENGTH,HEIGHT);
-	load.setPosition(LENGTH,HEIGHT+100);
-	settings.setPosition(LENGTH,HEIGHT+200);
-	exit.setPosition(LENGTH,HEIGHT+300);
+	title.setPosition((LENGTH-100)*k,10*k);
+	new1.setPosition(LENGTH*k,HEIGHT*k);
+	load.setPosition(LENGTH*k,(HEIGHT+100)*k);
+	settings.setPosition((LENGTH)*k,(HEIGHT+200)*k);
+	exit.setPosition(LENGTH*k,(HEIGHT+300)*k);
 	while (window.isOpen()) 
 	{
 		float time = clock.getElapsedTime().asMicroseconds();
